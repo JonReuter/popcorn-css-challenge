e2884437290ace95eb5cdcffdaf83a2e0a9d515b
unknown <you@example.com>
2019-12-09 21:30:41 -0500

    draft 3

---
+++ index.js
@@ -0,0 +1,130 @@
+/*
+// data types
+
+//bool
+var variableName = false;
+var bool2 = true;
+
+//int
+var num = 5;
+
+//float
+var floatNum = 1.23;
+
+//string 
+
+var word = 'a string';
+
+//zero index
+word[0];
+//console.log ('word[0]:', word[0]);
+
+//methods on data types
+
+word.indexOf('a');
+//console.log("word.indexOf('r'):", word.indexOf('r') );
+
+//arrays
+/*
+var arr = [];
+
+arr.push(4);
+arr.push(5);
+arr.push(6);
+arr.push(7);
+console.log(arr)
+arr.pop();
+console.log(arr,arr.length)
+
+//objects key value pair
+
+//"key": value
+
+var obj = {
+    key: value,
+    bool: true,
+    num:3,
+    str:'string',
+    char'a',
+    arr:{},
+    obj:{},
+};
+
+obj.num;
+obj[list-item-1];
+
+
+
+//for loop
+// for (init var; condition; increm)
+
+var arr = [];
+
+arr.push(4);
+arr.push(5);
+arr.push(6);
+
+for (var i = 0; i < arr.length; i++){
+
+    console.log('array', arr[i]);
+}
+
+
+var obj = {
+
+    bool: true,
+    num:3,
+    str:'string',
+    char:'a',
+    arr:[],
+    obj:{},
+};
+
+//loop through obj
+for (var key in obj){
+console.log('key value', obj[key]);
+
+};
+
+
+
+//pattern for storing data - array of objects 
+
+var todos = [
+    {id: 1, todo: 'take out trash'},
+    {id: 2, todo: 'do dishes'},
+    {id: 3, todo: 'make bed'}
+];
+
+for (var i =0; i < todos.length; i++){
+    console.log('todo list:', todos[i]['todo']);
+
+};
+
+*/
+
+var todos = [
+    {id: 1, todo: 'take out trash'},
+    {id: 2, todo: 'do dishes'},
+    {id: 3, todo: 'make bed'}
+    ],
+    [
+    {id: 1, todo: 'take out trash'},
+    {id: 2, todo: 'do dishes'},
+    {id: 3, todo: 'make bed'}
+    ],
+    [
+    {id: 1, todo: 'take out trash'},
+    {id: 2, todo: 'do dishes'},
+    {id: 3, todo: 'make bed'}
+    ]
+];
+
+//for (var i =0; i < todos.length; i++){
+    console.log('todo list:', todos[0]);
+
+//};
+
+
+
+
